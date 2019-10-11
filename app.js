8d3496e3d7d19346198b4b7b981cb5cc5a8f632c
gasparpeter <petihappygaspar@gmail.com>
2019-10-11 12:26:02 +0300

    - home

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let manOption = document.getElementById('manOption');
 let womanOption = document.getElementById('womanOption');
 
+let home = document.getElementById('home');
+
 let mansContainer = document.getElementById('mans');
 let womansContainer = document.getElementById('womans');
 
@@ -10,15 +12,31 @@ logo.addEventListener('click', function () {
    location.reload();
 });
 
+home.addEventListener('click', function () {
+    location.reload();
+});
+
 function womans() {
     mansContainer.style.display = 'none';
     womansContainer.style.display = 'block';
+    clearActiveMenuIcon();
+    womanOption.classList.add('active');
 }
 
 function mans() {
     womansContainer.style.display = 'none';
     mansContainer.style.display = 'block';
+    clearActiveMenuIcon();
+    manOption.classList.add('active');
 }
 
 manOption.addEventListener('click', mans);
 womanOption.addEventListener('click', womans);
+
+function clearActiveMenuIcon() {
+    const items = document.getElementsByClassName('menuIcon');
+
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.remove('active')
+    }
+}
